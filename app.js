ed1a87747448e487dab1049ae33d1446a7213de0
Art <simf.i@ya.ru>
2023-07-02 23:59:23 +0300

    cors edit4

--- app.js
+++ app.js
@@ -15,18 +15,7 @@ mongoose.connect(MONGO_ADRESS);
 app.use(requestLogger);
 app.use(limiter);
 app.use(helmet());
-app.use(cors({
-  origin: [
-    'http://127.0.0.1:27017',
-    'http://localhost:3001',
-    'http://localhost:3000/',
-    'http://artmovies.nomoredomains.rocks/',
-    'http://api.artmovies.nomoredomains.rocks/',
-    'https://artmovies.nomoredomains.rocks/',
-    'https://api.artmovies.nomoredomains.rocks/',
-  ],
-  method: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use('/', router);
